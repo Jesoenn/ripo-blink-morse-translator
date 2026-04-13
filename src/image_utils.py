ec00820afd7592973eaaa33ed3664542ec9b9fd7
Jakub Firlej <147878425+Jesoenn@users.noreply.github.com>
2026-04-13 18:28:52 +0200

    Refactor import statement for config module

--- src/image_utils.py
+++ src/image_utils.py
@@ -1,8 +1,7 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-# import config
-import src.config as config
+import config
 
 def apply_gamma_correction(frame, gamma):
     inv_gamma = 1.0 / gamma
